15adcb8003760c89ca8574cd7a3ee9ab2c9cbd09
YellowSamsung <yellows.submarin@yahoo.fr>
2020-01-03 13:06:34 +0200

    Added test_sphere_convhull

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ A Python module of various spherical computational geometry related functions.
 This module is a work-in-progress including various spherical geometry functions that I needed and could not find in the well-known libraries.
 The functions work on sets of points on the unit sphere.
 
-The list of existing functions (to be updated):
+The list of existing functions (some do not exist yet):
 
 -`sphere_convhull` - Computes the spherical convex hull of a set of points on the unit sphere (unfinished)
+
 -`pspolydist` - Computes the minimum distance from a point to a polyline on a sphere (unfinished). This is an adaptation to Python of the original MATLAB function [`p_spoly_dist`](https://www.mathworks.com/matlabcentral/fileexchange/52734-p_spoly_dist) by Michael Yoshpe.
+
+-`insphericalpolygon` - Returns points inside of spherical polygon.
 
 ## Some theoretical notions on spherical geometry
 ### Great circles
--- test_sphere_convhull.py
+++ test_sphere_convhull.py
@@ -1,13 +1,49 @@
-# import random
+import random
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from itertools import product, combinations
+
 #TODO
 
-# u = random.random()
-# v = random.random()
+lons = np.array([])
+lats = np.array([])
+number_of_points = 20
 
-# # convert to spherical coordinates
-# theta = 2 * math.pi * u # range [0,2pi)
-# phi = math.acos(2 * v - 1) # range (0, pi]
+for jj in range(0, number_of_points):
+    u = random.random()
+    v = random.random()
 
-# # convert to lng,lat
-# lng = theta / (2 * math.pi) * 360 - 180
-# lat =   phi / (2 * math.pi) * 360 -  90
+    # convert to spherical coordinates
+    theta = 2 * np.pi * u # range [0,2pi)
+    phi = np.arccos(2 * v - 1) * 0.5 # range (0, pi/2]
+
+    # convert to lng,lat
+    lng = theta / (2 * np.pi) * 360 - 180
+    lat =   phi / (2 * np.pi) * 360 
+
+    lons = np.append(lons, lng)
+    lats = np.append(lats, lat)
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_aspect("equal")
+
+# draw sphere
+u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+x = np.cos(u)*np.sin(v)
+y = np.sin(u)*np.sin(v)
+z = np.cos(v)
+ax.plot_wireframe(x, y, z, color="r")
+
+earthRadius =1 # 6378.137;
+lonr = np.deg2rad(lons)
+latr = np.deg2rad(lats)
+pts_x = earthRadius * np.cos(latr)*np.cos(lonr)
+pts_y = earthRadius * np.cos(latr)*np.sin(lonr)
+pts_z = earthRadius * np.sin(latr)
+
+ax.scatter(pts_x, pts_y, pts_z, color="g", s=100)
+
+plt.show()
